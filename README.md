03c8a34add652ee2475abc6006bf81923c308626
fernando <fernandomagalhaes@yahoo.com.br>
2023-01-27 19:59:45 -0300

    Sketching structure

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p align="center">
-<img src="sewrex.png" class="img-responsive" alt="sewrex logo">
+<img src="sewrex.gif" class="img-responsive" alt="sewrex logo">
 </p>
 
 **sewrex** (Search With Regular Expression) is a python script to be executed in the console, performing search in text files with regular expression. Sewrex uses only python built in libraries.
--- sewrex.py
+++ sewrex.py
@@ -1,4 +1,47 @@
+import re
+from enum import Enum
+
+
+class Color(str, Enum):
+    RESET = '\033[0m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    BLUE = '\033[34m'
+    YELLOW = '\033[93m'
+
+
+# TODO: Função Print
+
+class Sewrex:
+    def __init__(self, source, pattern):
+        self.source = source
+        self.pattern = pattern
+
+    def search(self):
+        with open(file=self.source, mode='r') as f:
+            text = f.readlines()
+            # TODO: Erro de não ler linhas
+
+        found = list()
+
+        for line in text:
+            search = re.finditer(pattern=self.pattern, string=line)
+
+            for result in search:
+
+                # line[result.start():result.end()]
+                a = 1
+            a = 1
+
+
+# TODO: Comentários inglês, explicação conforme um padrão
 
 
 if __name__ == "__main__":
-    a = 1
+
+    s = 'test.txt'
+    p = '[0-9]{4}\-[0-9]{4}'
+
+    sewrex = Sewrex(source=s, pattern=p)
+    sewrex.search()
+
