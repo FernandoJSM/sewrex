50059f3aa6e4812c368883db2195bbd5ffd2dbc5
fernando <fernandomagalhaes@yahoo.com.br>
2023-01-28 18:08:43 -0300

    Added print function

--- sewrex.py
+++ sewrex.py
@@ -8,6 +8,7 @@ class Color(str, Enum):
     GREEN = '\033[32m'
     BLUE = '\033[34m'
     YELLOW = '\033[93m'
+    GREY = '\033[37m'
 
 
 # TODO: Função Print
@@ -16,13 +17,15 @@ class Sewrex:
     def __init__(self, source, pattern):
         self.source = source
         self.pattern = pattern
+        self.text_highlighted = None
+        self.highlight_lines = list()
 
     def search(self):
         with open(file=self.source, mode='r') as f:
-            text = f.readlines()
+            text = f.read().splitlines()
             # TODO: Erro de não ler linhas
 
-        text_highlighted = text.copy()
+        self.text_highlighted = text.copy()
 
         found = list()
 
@@ -42,8 +45,20 @@ class Sewrex:
                 new_line += Color.YELLOW + line[result.start():result.end()] + Color. RESET
                 new_line += split[next_section]
                 next_section += 1
-            print(new_line)
-            a = 1
+                self.text_highlighted[i] = new_line
+                self.highlight_lines.append(i)
+
+    def print_result(self):
+        len_line = str(len(str(len(self.text_highlighted))) + 9)
+        for i, line in enumerate(self.text_highlighted):
+            row = Color.GREY + str(i) + Color.RESET
+            if i in self.highlight_lines:
+                row = Color.YELLOW + str(i) + Color.RESET
+            line_print = '{:>' + len_line + '} {}'
+            print(line_print.format(row, line))
+
+        # for line in text_highlighted:
+        #     print(line)
 
 
 # TODO: Comentários inglês, explicação conforme um padrão
@@ -56,4 +71,5 @@ if __name__ == "__main__":
 
     sewrex = Sewrex(source=s, pattern=p)
     sewrex.search()
+    sewrex.print_result()
 
